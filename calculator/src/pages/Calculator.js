07ed3963eac33e79c3f6c6b68654a1ed9413cbed
Akhmad <seehyuk2000@gmail.com>
2021-12-18 18:36:11 +0900

    Calculate Your pizza Calorie

---
+++ calculator/src/pages/Calculator.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../images/logo.svg";
+import calculator from "../images/calculator.svg";
+import Input from "../components/Input";
+import { defaultOptions } from "../utils";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = defaultOptions();
+  }
+
+  handlePizzaWeightChange = e => {
+    this.setState({ weight: e.target.value });
+  };
+
+  handlePizzaCountChange = e => {
+    this.setState({ count: e.target.value });
+  };
+
+  buildParameters() {
+    const opts = {
+      ...defaultOptions(),
+      weight: this.state.weight,
+      count: this.state.count,
+    };
+    return `weight=${opts.weight}&count=${opts.count}&water=${opts.water}&salt=${opts.salt}&sourdoughPercent=${opts.sourdoughPercent}&sourdoughHydration=${opts.sourdoughHydration}&dryYeastPercent=${opts.dryYeastPercent}`
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1 className="logo">
+            <img src={logo} alt="Pizza Dough" />
+          </h1>
+          <h2 className="logo">
+            <img src={calculator} alt="Calculator" />
+          </h2>
+        </header>
+        <main className="main">
+          <form>
+            <Input
+              id="count"
+              onChange={this.handlePizzaCountChange}
+              label="Number of pizzas"
+              value={this.state.count}
+              step="1"
+            />
+
+            <Input
+              id="weight"
+              onChange={this.handlePizzaWeightChange}
+              label="Weight per pizza (grams)"
+              value={this.state.weight}
+            >
+              <small className="text-muted">
+                1) Pick the amount of pizzas you want to bake and your desired final dough weight.<br />
+                2) Choose between a yeast based dough or sourdough based dough.
+              </small>
+            </Input>
+
+            <div className="calculate-buttons">
+              <Link to={`result?yeast=true&${this.buildParameters()}`}>Yeast</Link>
+              <span>
+                - or -
+              </span>
+              <Link to={`result?sourdough=true&${this.buildParameters()}`}>Sourdough</Link>
+            </div>
+          </form>
+          <div className="more-info">
+            <h1>Background</h1>
+            <p>
+                Typically the pizza dough weighs between 200 and 250 grams. For a home oven 200 grams
+                is a recommended size. Change this value depending on the size of your oven.
+            </p>
+            <p>A real Neapolitan pizza consists of only flour water and salt. Traditionally sourdough has been used. Sourdough requires
+              more advanced dough handling. Thus for beginners the yeast based dough is recommended.
+            </p>
+            <p>
+              The key is to have all the ingredients in the exact quantity. To calculate
+              how much of each ingredient is used "Baker's Math" is applied. This means
+              the ingredients are calculated based on the amount of flour that you are
+              using. So lets say the ingredient would say 60%, that would mean per
+              every 100 grams of flour you need to add 60 grams of water. This way you
+              can easily scale up the amount of pizzas that you want to bake.
+            </p>
+            <div className="more-info-links">
+              <a href="https://github.com/JooSeeHyuk"><span role="img" aria-label="Book">🧐</span> My github</a>
+              <a href="https://t.me/AhmadRamazonovich">Telegram</a>
+              <a href="https://www.instagram.com/joseehyuk/">Instagram</a>
+              <a href="https://www.facebook.com/Joo.See.Hyuk.7/">Facebook</a>
+            </div>
+          </div>
+          <div className="text-muted pwa" id="pwa">
+            Add to homescreen
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
